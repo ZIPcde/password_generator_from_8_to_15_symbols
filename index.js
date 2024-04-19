148a84e4e236cf3f7bbb8dc5b7a74fcaf62755a7
ZIPcde <votmoyyashik100@gmail.com>
2024-04-19 10:51:52 +0300

    converted into a module

--- index.js
+++ index.js
@@ -1,30 +1,34 @@
 const readline = require('readline');
 const crypto = require('crypto');
 
-const rl = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout
-});
+function generatePassword() {
+    const rl = readline.createInterface({
+        input: process.stdin,
+        output: process.stdout
+    });
 
-rl.question('Введите число от 8 до 15: ', (answer) => {
-    const passwordLength = parseInt(answer);
-    if (passwordLength >= 8 && passwordLength <= 15) {
-        const lowercaseLetters = 'abcdefghijklmnopqrstuvwxyz';
-        const uppercaseLetters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
-        const specialCharacters = '!@#$%^&*()_+[]{}|;:,.<>?';
+    rl.question('Введите число от 8 до 15: ', (answer) => {
+        const passwordLength = parseInt(answer);
+        if (passwordLength >= 8 && passwordLength <= 15) {
+            const lowercaseLetters = 'abcdefghijklmnopqrstuvwxyz';
+            const uppercaseLetters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+            const specialCharacters = '!@#$%^&*()_+[]{}|;:,.<>?';
 
-        let allCharacters = lowercaseLetters + uppercaseLetters + specialCharacters;
-        let password = '';
+            let allCharacters = lowercaseLetters + uppercaseLetters + specialCharacters;
+            let password = '';
 
-        for (let i = 0; i < passwordLength; i++) {
-            let randomIndex = crypto.randomInt(0, allCharacters.length);
-            password += allCharacters[randomIndex];
+            for (let i = 0; i < passwordLength; i++) {
+                let randomIndex = crypto.randomInt(0, allCharacters.length);
+                password += allCharacters[randomIndex];
+            }
+
+            console.log('Сгенерированный пароль:', password);
+        } else {
+            console.log('Введенное число не соответствует числовому диапазону от 8 до 15.');
         }
 
-        console.log('Сгенерированный пароль:', password);
-    } else {
-        console.log('Введенное число не соответствует числовому диапазону от 8 до 15.');
-    }
+        rl.close();
+    });
+}
 
-    rl.close();
-});
+module.exports = { generatePassword };
